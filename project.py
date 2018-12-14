b66927a5e7005fef79cde740c6e7fd32d3da95f9
gnems <g_nemsveridze@cu.edu.ge>
2018-12-14 05:46:30 +0400

    Caesar's cipher

---
+++ project.py
@@ -0,0 +1,20 @@
+def caesar():
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    
+    message = input("Enter your message: ")
+    key = int(input("Enter your key: "))
+    
+    messageLength = len(message)
+    
+    cipheredText = ""
+    
+    for i in range(messageLength):
+        character = message[i]
+        location = alphabet.find(character)
+        newLocation = (location + key) % 26
+        cipheredText += alphabet[newLocation]
+        
+    print(cipheredText)
+    
+    
+caesar()
