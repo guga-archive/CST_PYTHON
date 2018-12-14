2da32b2727d042f0fe0c4fd39f53059743cc98af
gnems <g_nemsveridze@cu.edu.ge>
2018-12-14 06:02:44 +0400

    Add decryption

--- project.py
+++ project.py
@@ -16,5 +16,24 @@ def caesar():
         
     print(cipheredText)
     
+def caesarDecrypt():
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    
+    message = input("Enter your message: ")
+    key = int(input("Enter your key: "))
+    
+    messageLength = len(message)
+    
+    cipheredText = ""
+    
+    for i in range(messageLength):
+        character = message[i]
+        location = alphabet.find(character)
+        newLocation = (location - key) % 26
+        cipheredText += alphabet[newLocation]
+        
+    print(cipheredText)
+    
     
 caesar()
+caesarDecrypt()
